1b59dea728970953dec899aa8a2adcafa74fd326
pburkett <pburkett636@gmail.com>
2024-01-02 19:41:30 -0700

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
 import * as React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { DarkModeButton } from './ui/DarkModeButton';
-import { GitHubIconLink } from './ui/GitHubIconLink';
-import { globalCss, styled } from './stitches.config';
-
-const AppContainer = styled('div', {
-  maxWidth: '540px',
-  padding: '12px 15px 25px',
-  margin: '0 auto',
-});
 
 export const App: React.VFC = () => {
-  globalCss();
-
-  return (
-    <AppContainer>
-      <h1>Coming soon</h1>
-    </AppContainer>
-  );
+  return <h1>Coming soon</h1>;
 };
